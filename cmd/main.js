0ff7c60ebd95375360fa30389a99ee9c855adbdf
=Aekarattakhat <=aekarattakhat.s@kkumail.com>
2025-11-28 11:35:24 +0700

    remove test

--- cmd/main.js
+++ cmd/main.js
@@ -11,6 +11,7 @@ app.get("/", (req, res) => {
   res.json({ message: "Hello API" });
 });
 
+
 app.listen(process.env.PORT || 3000, () =>
   console.log(`Server running on port ${process.env.PORT || 3000}`)
 );
