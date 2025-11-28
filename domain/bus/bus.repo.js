8504af17ad2230bb8b475836ba4301c03bf07d1e
ApichaiTi <81065469+artijo@users.noreply.github.com>
2025-11-28 13:17:03 +0700

    add Helper utility for date and time manipulations

--- domain/bus/bus.repo.js
+++ domain/bus/bus.repo.js
@@ -4,11 +4,11 @@ export default class BusRepo {
   constructor() {}
 
   async findBus(id) {
-      const db = getDBFromContext()
-      return await db.bus.findUnique({
-        where: {
-          bus_id: parseInt(id),
-        },
-      })
+    const db = getDBFromContext();
+    return await db.bus.findUnique({
+      where: {
+        bus_id: parseInt(id),
+      },
+    });
   }
 }
