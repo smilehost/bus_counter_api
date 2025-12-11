442adff7cc93b5c32d507319d71a11a6784c8955
ARTIJO <a@artijo.com>
2025-12-11 16:01:19 +0700

    Refactor camera routes and handlers; update seeder for initial data population

--- domain/camera/camera.repo.js
+++ domain/camera/camera.repo.js
@@ -3,32 +3,17 @@ import db from "../../prisma/client.js";
 export default class CameraRepo {
   constructor() {}
   //   ตัวอย่างฟังก์ชันในการดึงข้อมูลกล้องจากฐานข้อมูล
-  async findCamera(id) {
-    return await db.camera_device.findUnique({
-      where: {
-        camera_id: parseInt(id),
-        deleted_at: null,
-      },
-    });
-  }
-  async findCameras() {
-    return await db.camera_device.findMany({
-      where: {
-        deleted_at: null,
-      },
-    });
-  }
-  async findInstallationByCameraId(camera_id) {
-    return await db.installed_camera.findFirst({
-      where: {
-        installed_camera_id: parseInt(camera_id),
-        deleted_at: null,
-      },
-    });
-  }
   async InstallationCameras() {
     return await db.installed_camera.findMany({
       where: {
+        deleted_at: null,
+      },
+    });
+  }
+  async getInstallationById(id) {
+    return await db.installed_camera.findUnique({
+      where: {
+        install_id: parseInt(id),
         deleted_at: null,
       },
     });
