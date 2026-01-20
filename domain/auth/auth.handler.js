3f5ed9a598a4a133178836b056b91bf43bd31329
ARTIJO <a@artijo.com>
2026-01-20 15:13:08 +0700

    Add token verification schema and refactor login logic to use environment variable for AUTH_URL

--- domain/auth/auth.handler.js
+++ domain/auth/auth.handler.js
@@ -1,6 +1,7 @@
 import { generateToken } from "../../util/jwt.js";
 import { AppError } from "../../util/error.js";
 import ResponseFormatter from "../../util/response.js";
+import { verifyTokenSchema } from "../../util/validator.js";
 
 export default class AuthHandler {
   constructor({ authService }) {
@@ -15,18 +16,17 @@ export default class AuthHandler {
   }
 
   async Login(req, res) {
-    const { code, service, session_id } = req.body;
-    console.log("Login request received with:", { code, service, session_id });
+    const { code, service, session_id } = verifyTokenSchema.parse(req.body);
     try {
       const response = await fetch(
-        `https://authen-center.lab.bussing.app/api/v1/login/verify`,
+        `${process.env.AUTH_URL}/api/v1/login/verify`,
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
           body: JSON.stringify({ code, service, session_id }),
-        }
+        },
       );
 
       const data = await response.json();
@@ -44,13 +44,13 @@ export default class AuthHandler {
         // Create access token (expires in 1 day)
         const access_token = generateToken(
           { account_id, account_username, com_id, account_role },
-          "1d"
+          "1d",
         );
 
         // Create refresh token (expires in 7 days)
         const refresh_token = generateToken(
           { account_id, session_id: authSessionId },
-          "7d"
+          "7d",
         );
 
         res.json(
@@ -59,7 +59,7 @@ export default class AuthHandler {
             refresh_token,
             account_name,
             com_id,
-          })
+          }),
         );
       } else {
         res.status(401).json(ResponseFormatter.error("Authentication failed"));
